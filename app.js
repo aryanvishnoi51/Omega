38a46e44c436576386454fc91078489fb8d237d6
Aryan Vishnoi <68261488+aryanvishnoi51@users.noreply.github.com>
2020-08-07 11:14:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyDeDlLjXXdr05MJCnWZiCY12DK_DtmXpcc",
+  authDomain: "insta-hack-f79dd.firebaseapp.com",
+  databaseURL: "https://insta-hack-f79dd.firebaseio.com",
+  projectId: "insta-hack-f79dd",
+  storageBucket: "insta-hack-f79dd.appspot.com",
+  messagingSenderId: "692963019556",
+  appId: "1:692963019556:web:353cb5f72e34864cd681b1",
+  measurementId: "G-5VM6D2H3G3"
 };
-
      
  
 ////Do Not Edit Below This
